73eaf45f1b41a13fdd781d9e7e43626c08b2da9e
Alexander Knue <ak4906@columbia.edu>
2026-01-18 05:12:22 -0500

    Initial Commit

---
+++ hanzipracticeapp/hanzipracticeappApp.swift
@@ -0,0 +1,17 @@
+//
+//  hanzipracticeappApp.swift
+//  hanzipracticeapp
+//
+//  Created by Alexander Knue on 1/18/26.
+//
+
+import SwiftUI
+
+@main
+struct hanzipracticeappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
